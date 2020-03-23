8f21705ec56d94b3f1c37bb652c1b98f89258b81
D1-92 <wyettlei@hotmail.com>
2020-03-23 10:51:21 +0800

    jedis sentinel test

---
+++ crud-redis/src/main/java/com/wyett/redis/JedisSentinelTest.java
@@ -0,0 +1,53 @@
+package com.wyett.redis;
+
+import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.jedis.JedisSentinelPool;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author : wyettLei
+ * @date : Created in 2020/3/12 17:46
+ * @description: TODO
+ */
+
+public class JedisSentinelTest {
+    public static void main(String[] args) {
+        // jedis pool config
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        jedisPoolConfig.setMaxTotal(20);
+        jedisPoolConfig.setMaxIdle(10);
+        jedisPoolConfig.setMinIdle(5);
+
+        String myMaster = "mymaster";
+
+        Set<String> sentinelSet = new HashSet<String>();
+        sentinelSet.add(new HostAndPort("10.18.56.30", 26379).toString());
+        sentinelSet.add(new HostAndPort("10.19.58.18", 26379).toString());
+        sentinelSet.add(new HostAndPort("10.18.56.149", 26379).toString());
+
+        // jedisSentinelPool并非与java与sentinel建立connection，而是通过jedisSentinelPool发现master节点，并与redis建立链接
+        JedisSentinelPool jedisSentinelPool
+                = new JedisSentinelPool(myMaster, sentinelSet, jedisPoolConfig, 3000, "123456789");
+
+        Jedis jedis = null;
+
+        try {
+            jedis = jedisSentinelPool.getResource();
+
+            jedis.set("computer:brand", "thinkpad");
+            System.out.println(jedis.get("computer:brand"));
+        } finally {
+            if (jedis != null) {
+                jedis.close();
+            }
+        }
+
+
+
+    }
+
+}
