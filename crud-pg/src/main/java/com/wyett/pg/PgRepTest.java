5a40bd985f0b3601a686ba2ed1b840085157f78e
D1-92 <wyettlei@hotmail.com>
2020-03-31 18:19:36 +0800

    redis lock

---
+++ crud-pg/src/main/java/com/wyett/pg/PgRepTest.java
@@ -0,0 +1,13 @@
+package com.wyett.pg;
+
+/**
+ * @author : wyettLei
+ * @date : Created in 2020/3/27 11:50
+ * @description: TODO
+ */
+
+public class PgRepTest {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ crud-redis/src/main/java/com/wyett/redis/lock/RedisLockApplication.java
@@ -0,0 +1,38 @@
+package com.wyett.redis.lock;
+
+import org.redisson.Redisson;
+import org.redisson.config.Config;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : wyettLei
+ * @date : Created in 2020/3/31 16:12
+ * @description: TODO
+ */
+
+@SpringBootApplication
+public class RedisLockApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RedisLockApplication.class, args);
+
+    }
+
+    public Redisson redisson() {
+        Config config = new Config();
+        // single
+        config.useSingleServer().setAddress("redis://xxxxxxxxxxx:6379").setDatabase(0);
+
+        // cluster
+        /*
+        config.useClusterServers()
+                .addNodeAddress("redis://xxxxxxxxxx:6379")
+                .addNodeAddress("redis://xxxxxxxxxx:6379")
+                .addNodeAddress("redis://xxxxxxxxxx:6379");
+         */
+
+        return (Redisson) Redisson.create(config);
+
+
+    }
+}
